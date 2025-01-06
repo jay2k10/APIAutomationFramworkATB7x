f075076871a5dd8930608cce19b6322bd13796ca
jay shankar <jayshankarprasad57@gmail.com>
2025-01-06 14:16:42 +0530

    All file pushed!

--- src/main/java/com/testingacademy/modules/PayloadManager.java
+++ src/main/java/com/testingacademy/modules/PayloadManager.java
@@ -1,5 +1,65 @@
 package com.testingacademy.modules;
 
+import com.github.javafaker.Faker;
+import com.google.gson.Gson;
+import com.testingacademy.pojos.Booking;
+import com.testingacademy.pojos.BookingDates;
+import com.testingacademy.pojos.BookingResponse;
+
 public class PayloadManager {
+    Gson gson;
+
     //Serialization and deserialization
+    public String createPayloadBookingAsString(){
+        Booking booking = new Booking();
+        booking.setFirstname("James");
+        booking.setLastname("Brown");
+        booking.setTotalprice(111);
+        booking.setDepositpaid(true);
+
+        BookingDates bookingdates = new BookingDates();
+        bookingdates.setCheckin("2024-02-01");
+        bookingdates.setCheckout("2024-02-01");
+
+        booking.setBookingdates(bookingdates);
+        booking.setAdditionalneeds("Breakfast");
+
+        System.out.println("Booking id is 1:"+booking);
+        // Java Object -> JSON String (byteStream) - Serlization
+        gson = new Gson();
+        String jsonStringpayload = gson.toJson(booking);
+        System.out.println(jsonStringpayload);
+        return jsonStringpayload;
+    }
+    public String createPayloadBookingAsStringFaker(){
+        Faker faker = new Faker();
+        Booking booking = new Booking();
+        booking.setFirstname(faker.name().firstName());
+        booking.setLastname(faker.name().lastName());
+        booking.setTotalprice(faker.random().nextInt(1000));
+        booking.setDepositpaid(true);
+
+        BookingDates bookingdates = new BookingDates();
+        bookingdates.setCheckin("2024-02-01");
+        bookingdates.setCheckout("2024-02-01");
+
+        booking.setBookingdates(bookingdates);
+        booking.setAdditionalneeds("Breakfast");
+
+        System.out.println("Booking id is 2:"+booking);
+        // Java Object -> JSON String (byteStream) - Serlization
+        gson = new Gson();
+        String jsonStringpayload = gson.toJson(booking);
+        System.out.println(jsonStringpayload);
+        return jsonStringpayload;
+
+
+    }
+    public BookingResponse bookingResponseJava(String responseString) {
+        gson = new Gson();
+        BookingResponse bookingResponse = gson.fromJson(responseString, BookingResponse.class);
+        return bookingResponse;
+    }
+
+
 }
--- src/test/java/com/testingacademy/tests/crud/TestCreateBookingPOST.java
+++ src/test/java/com/testingacademy/tests/crud/TestCreateBookingPOST.java
@@ -1,19 +1,48 @@
 package com.testingacademy.tests.crud;
 
+import com.testingacademy.base.BaseTest;
+import com.testingacademy.endpoints.APIConstants;
+import com.testingacademy.pojos.BookingResponse;
+
 import io.qameta.allure.*;
+import io.restassured.RestAssured;
+
+import org.hamcrest.Matchers;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class TestCreateBookingPOST {
+import static org.assertj.core.api.Assertions.*;
 
-    @Link(name="Website", url="https://hdor.com/")
+
+public class TestCreateBookingPOST extends BaseTest {
+
+    @Link(name = "Website", url = "https://hdor.com/")
     @Issue("Post-001")
     @TmsLink("TMS-001")  //Test management system
     @Severity(SeverityLevel.CRITICAL)
     @Owner("Jay Shankar")
     @Description("Verify the post request is working fine!!")
     @Test
-    public void testVerifyCreateBookingPost(){
+    public void testVerifyCreateBookingPost() {
+
+        requestSpecification.basePath(APIConstants.CREATE_UPDATE_BOOKING_URL);
+        response = RestAssured.given(requestSpecification).when()
+                .body(payloadManager.createPayloadBookingAsString()).post();
+        validatableResponse = response.then().log().all();
+        validatableResponse.statusCode(200);
+
+        //Default Rest Assured
+        validatableResponse.body("booking.firstname", Matchers.equalTo("James"));
+
+        BookingResponse bookingResponse = payloadManager.bookingResponseJava(response.asString());
+
+        // AssertJ
+        assertThat(bookingResponse.getBookingid()).isNotNull();
+        assertThat(bookingResponse.getBooking().getFirstname()).isNotNull().isNotBlank();
+        assertThat(bookingResponse.getBooking().getFirstname()).isEqualTo("James");
+
+
+        //TestNG Assertions
         Assert.assertEquals(true, true);
     }
 
